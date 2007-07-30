ea9dca83f8a2a0d6f1e25f1b0a93d5d834b02a26
deepblue <deepblue@8a3e2545-6861-4513-bbc0-502abaebb7ec>
2007-07-30 16:19:38 +0000

    Site.list_per_page_atom setting added
    git-svn-id: svn+ssh://rubyforge.org/var/svn/springnote/slugger@5 8a3e2545-6861-4513-bbc0-502abaebb7ec

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   caches_action :index
   
   def index
-    @page_no, list_per_page = request.format.atom? ? [1, 10] : [(params[:no] || 1).to_i, Site.list_per_page]
+    @page_no, list_per_page = request.format.atom? ? [1, Site.list_per_page_atom] : [(params[:no] || 1).to_i, Site.list_per_page]
     @pages = Page.list[(@page_no-1)*list_per_page...(@page_no)*list_per_page].map{|id| Page.find(id)}
 
     respond_to do |format|      
