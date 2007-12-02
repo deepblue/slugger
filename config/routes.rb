78c110cbb1b8b2b9f5f68bfa3c3d13bbf9fa64eb
deepblue <deepblue@8a3e2545-6861-4513-bbc0-502abaebb7ec>
2007-12-02 13:21:19 +0000

    page has many trackabacks (not has_one)
    git-svn-id: svn+ssh://rubyforge.org/var/svn/springnote/slugger@34 8a3e2545-6861-4513-bbc0-502abaebb7ec

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
-  # The priority is based upon order of creation: first created -> highest priority.
-  
-  map.resources :pages, :has_many => :comments, :has_one => :trackback
+  map.resources :pages, :has_many => [:comments, :trackbacks]
   
   map.home    '/',      :controller => 'pages',  :action => 'index'
   map.connect 'xmlrpc', :controller => 'xmlrpc', :action => 'api',  :conditions => {:method => :post}
