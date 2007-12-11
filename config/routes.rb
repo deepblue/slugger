05bde31dd5021ad46aada1352a8a3ca654c39d1b
deepblue <deepblue@8a3e2545-6861-4513-bbc0-502abaebb7ec>
2007-12-11 04:55:41 +0000

    change url /pages/search to /pages?q=keyword
    git-svn-id: svn+ssh://rubyforge.org/var/svn/springnote/slugger@40 8a3e2545-6861-4513-bbc0-502abaebb7ec

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resources :pages, :has_many => [:comments, :trackbacks], :collection => {:search => :get}
+  map.resources :pages, :has_many => [:comments, :trackbacks]
   
   map.home    '/',      :controller => 'pages',  :action => 'index'
   map.connect 'xmlrpc', :controller => 'xmlrpc', :action => 'api',  :conditions => {:method => :post}
