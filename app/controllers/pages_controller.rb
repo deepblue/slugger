588d7bc2c19f05debc08d00e236331b680a96587
deepblue <deepblue@8a3e2545-6861-4513-bbc0-502abaebb7ec>
2007-12-11 05:05:27 +0000

    rollback changes. /pages;q=xxx doesn't work because of action_caches.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/springnote/slugger@41 8a3e2545-6861-4513-bbc0-502abaebb7ec

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,6 @@ class PagesController < ApplicationController
   caches_action :index, :cache_path => Proc.new{|controller| controller.send(:pages_url, :no => controller.params[:no] || 1)}
   
   def index
-    return search if params[:q]
-    
     @page_no, list_per_page = request.format.atom? ? [1, Site.list_per_page_atom] : [(params[:no] || 1).to_i, Site.list_per_page]
     @articles = Page.list[(@page_no-1)*list_per_page...(@page_no)*list_per_page].map{|id| Page.find(id)}
 
@@ -12,20 +10,19 @@ class PagesController < ApplicationController
       format.atom { render :layout => false }
     end
   end
+  
+  def search
+    @search_string = params[:q]
+    @articles = Page.search(@search_string).select{|page| page.blog_post?}
     
+    render_template 'search'
+  end
+  
   def show    
     @article = page(params[:id])
     return render_404 unless @article
     @articles = [@article]    
     
     render_template 'single'
-  end
-  
-protected
-  def search
-    @search_string = params[:q]
-    @articles = Page.search(@search_string).select{|page| page.blog_post?}
-  
-    render_template 'search'
   end
 end
